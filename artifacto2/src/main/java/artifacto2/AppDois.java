8f293916cdf4c1167fc4239a02aeb37f4f59baea
qxx0533 <Luis.Loureiro@partner.bmw.de>
2019-01-09 14:50:19 +0000

    ttt

---
+++ artifacto2/src/main/java/artifacto2/AppDois.java
@@ -0,0 +1,14 @@
+package artifacto2;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class AppDois 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! App Dois" );
+    }
+}
